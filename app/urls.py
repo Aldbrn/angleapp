f234869ab58fa23045c014b6eb97936d3aeaddf0
Aldbrn <aldbrn.xxx@gmail.com>
2021-03-01 03:30:56 +0900

    add logout function

--- app/urls.py
+++ app/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
+from django.contrib.auth import views as auth_views
 from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
     path("signup/", views.signup, name="signup"),
     path("login/", views.loginfunc, name="login"),
-    path("logout/", views.logout, name="logout"),
+    path("logout/", auth_views.LogoutView.as_view(), name="logout"),
 ]
