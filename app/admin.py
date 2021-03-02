cf0f686f668dbcfc153f73f124f1f3ac3f0ca0c3
Aldbrn <aldbrn.xxx@gmail.com>
2021-03-02 22:57:41 +0900

    add Post model

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Post
 
 # Register your models here.
+
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ("id", "fish")
+    list_display_links = ("id", "fish")
+
+
+admin.site.register(Post, PostAdmin)
--- app/models.py
+++ app/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+
+class Post(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    image = models.ImageField(upload_to="photos")
+    fish = models.CharField(max_length=20)
+    size = models.PositiveIntegerField(null=True, blank=True)
+    comment = models.TextField(blank=True)
+    area = models.CharField(max_length=20)
+    created_at = models.DateField(auto_now=True)
+
+    def __str__(self):
+        return self.fish
